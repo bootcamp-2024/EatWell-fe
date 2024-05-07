cec07a9e0d7c5c9dcbb8eb4cbd026201dd569801
thanhthaothaothao2 <thanhthaothaothao2@gmail.com>
2024-05-07 13:03:54 +0700

    done ui homepage

---
+++ app/src/components/Landing/ServiceProvide/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Row, Col, Space, Avatar } from "antd";
+import { SmileOutlined, HeartOutlined, StarOutlined } from "@ant-design/icons";
+
+const ServiceProvide = () => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          <Row>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12}>
+          <Row>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col span={24}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div>
+                  <h4 style={{ fontWeight: 500 }}>
+                    Lorem ipsum dolor sit amet
+                  </h4>
+                  <p style={{ width: "70%", marginTop: "10px" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                    do eiusmod tempor incididunt ut labore et dolore.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ServiceProvide;
--- app/src/components/Landing/index.js
+++ app/src/components/Landing/index.js
@@ -2,6 +2,8 @@ import Banner from "../Banner";
 import { useLocation } from "react-router-dom";
 import InfoPanel from "./InfoPanel";
 import BusinessPlan from "./BussinessPlan";
+import "./style.css";
+import ServiceProvide from "./ServiceProvide";
 
 const Landing = (props) => {
   const location = useLocation();
@@ -18,10 +20,23 @@ const Landing = (props) => {
             fontWeight: 700,
           }}
         >
-          GIỚI THIỆU{" "}
+          GIỚI THIỆU
         </h3>
         <InfoPanel />
         <BusinessPlan />
+        <h3
+          style={{
+            textAlign: "center",
+            marginTop: "40px",
+            color: "#18AEAC",
+            fontWeight: 700,
+          }}
+        >
+          DỊCH VỤ MÀ CHÚNG TÔI CUNG CẤP
+        </h3>
+        <div style={{ marginLeft: "15%" }}>
+          <ServiceProvide style={{}} />
+        </div>
       </div>
     </section>
   );
