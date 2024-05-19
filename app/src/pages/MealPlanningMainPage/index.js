016394759b7f9b2b963bdef83a2ea63905949a03
nacon304 <nhatnguyen106225@gmail.com>
2024-05-19 13:18:27 +0700

    add code FE recognition

--- app/src/pages/MealPlanningMainPage/index.js
+++ app/src/pages/MealPlanningMainPage/index.js
@@ -7,6 +7,7 @@ import HeaderDashboard from "components/HeaderDashboard";
 import Analysis from "components/Analysis";
 import { Route, Routes } from "react-router-dom";
 import AccountPage from "pages/AccountPage";
+import Recognition from "components/Recognition";
 import Calendar from "components/Calendar";
 import MenuList from "components/MenuList";
 const { Sider, Header, Content } = Layout;
@@ -39,6 +40,7 @@ const MealPlanningMainPage = () => {
           <Routes>
             <Route path="/analysis" element={<Analysis />} />
             {/* <Route exact path="/menus" element={<MenuList />} /> */}
+            <Route path="/recognition" element={<Recognition />} />
             <Route exact path="/calendar" element={<MenuList />} />
             <Route path="/settings/*" element={<AccountPage />} />
           </Routes>
